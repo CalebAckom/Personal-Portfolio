941506fa34dd01d47f4a949580772777a9acb191
Caleb Ackom <caleb.ackom@amalitech.org>
2022-02-02 14:48:16 +0000

    Completed Services section

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,3 +79,32 @@ tabs.forEach(tab =>
             tab.classList.add('qualification__active')
         })
     })
+
+/*+++++ Services Tab +++++*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalCloses = document.querySelectorAll('.services__modal-close')
+
+let modal = function(modalClick)
+{
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) =>
+{
+    modalBtn.addEventListener('click', () =>
+    {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) =>
+{
+    modalClose.addEventListener('click', () =>
+    {
+        modalViews.forEach((modalView) =>
+        {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
